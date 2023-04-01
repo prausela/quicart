9a22239fe6294c516381234af7ecac9aaa0ee7af
Sofia Picasso <spicasso@itba.edu.ar>
2023-04-01 19:30:27 -0300

    Add products

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,48 @@
 import { useState } from 'react'
 
 function App() {
+  const [productList, setProductList] = useState(
+  [{id: 1, name: "Leche La Serenísima 1L"},
+      {id: 2, name: "Palta Hass 1u"},
+      {id: 3, name: "Vivere Suavizante Clásico"},
+      {id: 4, name: "Suprema de Pollo 1kg"},
+      {id: 5, name: "Lomo Premium 1kg"},
+      {id: 6, name: "Milanesa Nalga 1kg"},
+      {id: 7, name: "Bife de Chorizo 1kg"},
+      {id: 8, name: "Beefeater London Dry Gin 1L"},
+      {id: 9, name: "Aperitivo Campari 750cc"},
+      {id: 10, name: "Cerveza Heineken"},
+      {id: 11, name: "Oreo Galletas"},
+      {id: 12, name: "Finlandia Queso Untable"},
+      {id: 13, name: "Cebolla Blanca 1kg"},
+      {id: 14, name: "Limón 1u"},
+      {id: 15, name: "Pera 1u"},
+      {id: 16, name: "Yogurisimo Natural"},
+      {id: 17, name: "Leche Condensada"},
+      {id: 18, name: "Crema de Leche Tonadita 200cc"},
+      {id: 19, name: "Manteca La Serenísima"},
+      {id: 20, name: "Sprite Light 1L"},
+      {id: 21, name: "Coca-Cola Original 2.25l"},
+      {id: 22, name: "Arvejas Remojadas en Lata"},
+      {id: 23, name: "Ketchup Hellmann's"},
+      {id: 24, name: "Pure de Tomates"},
+      {id: 25, name: "Fideos Don Vincente"},
+      {id: 26, name: "Arroz Dos Hermanos"},
+      {id: 27, name: "Aceite de Girasol Pureza"},
+      {id: 28, name: "Vinagre de Alcohol"},
+      {id: 29, name: "Giacomo Capelletini Tricolor"},
+      {id: 30, name: "Dulce de Leche La Serenísima"},
+      {id: 31, name: "Miel Aleluya"},
+      {id: 32, name: "El Brocal Mermelada de Ciruela"},
+      {id: 33, name: "Alicante Orégano"},
+      {id: 34, name: "Sal Fina"},
+      {id: 35, name: "Playadito Yerba Mate"},
+      {id: 36, name: "Papel Higiénico Elite"},
+      {id: 37, name: "Procenex Limpiador Lavanda"},
+      {id: 38, name: "Jamón Cocido Paladini"},
+      {id: 39, name: "Malbec Alma Mora"},
+      {id: 40, name: "Pilas Duracell"},
+  ]);
 
   return (
     <></>
