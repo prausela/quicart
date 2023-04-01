1bb0c555d281f1c81ed719c8954c0d886d91acb8
Bri Rojas <brirojas@MacBook-Pro-de-solvd.local>
2023-04-01 18:40:04 -0300

    material theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import { useState } from 'react'
+import { Button, Typography } from '@mui/material'
 
 function App() {
 
   return (
-    <></>
+    <>
+    <Button variant="contained" color="primary"> Hola Mundo </Button>
+    <Button variant="outlined" color="secondary"> Hola Mundo </Button>
+    <Typography variant="h1">
+      Hola Mundo
+    </Typography>
+    <Typography variant="h2">
+      Hola Mundo
+    </Typography>
+    <Typography variant="p">
+      Hola Mundo
+    </Typography>
+    </>
   )
 }
 
