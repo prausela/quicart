7931b867530b6b0c15fc6d7840f0e6eb600eca29
BRISA ROJAS <rojaasbrisa@gmail.com>
2023-04-01 21:46:39 -0300

    saved lists component

---
+++ src/components/SavedLists.jsx
@@ -0,0 +1,35 @@
+import { Box, Typography, List } from '@mui/material';
+import { theme } from '../styles/theme';
+
+const SavedLists = ({ lists }) => {
+    return (
+        <List>
+            {lists.map((list) => (
+                <Typography variant="h2" sx={{
+                    border: '1px solid black',
+                    height: '50px',
+                    width: '90%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    'box-shadow': '-15px 11px 25px -1px ',
+                    '-webkit-box-shadow': '-15px 11px 25px -1px rgba(0,0,0,0.4)',
+                    '-moz-box-shadow': '-15px 11px 25px -1px rgba(0,0,0,0.4)',
+                    '&:hover': {
+                        backgroundColor: 'secondary.main',
+                        cursor: 'pointer',
+                        'box-shadow': '-15px 11px 25px -1px rgba(0,0,0,0.5)',
+                        '-webkit-box-shadow': '-15px 11px 25px -1px rgba(0,0,0,0.5)',
+                        '-moz-box-shadow': '-15px 11px 25px -1px rgba(0,0,0,0.5)',                        
+                    }
+                }}
+                    py={1}
+                    px={2}
+                    my={1}
+                >{list}</Typography>
+            ))}
+        </List>
+
+    );
+}
+
+export default SavedLists;
